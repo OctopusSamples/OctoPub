d0c10cc5b7ace84cb3f5bf306cc6f94baae85989
Matthew Casperson <matthewcasperson@gmail.com>
2021-10-27 09:15:36 +1000

    Working on github actions

---
+++ go/votes-service/pkg/models/vote.go
@@ -0,0 +1,12 @@
+package models
+
+import (
+	"time"
+)
+
+type Vote struct {
+	ID        int       `jsonapi:"primary,blogs"`
+	CreatedAt time.Time `jsonapi:"attr,created_at"`
+	IPAddress string    `jsonapi:"attr,ip_address"`
+	Product   *Product  `jsonapi:"relation,product"`
+}
