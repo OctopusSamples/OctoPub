d658f23c1d8231683ab2445644b7a8059e1c228a
Matthew Casperson <matthewcasperson@gmail.com>
2021-10-27 14:04:12 +1000

    More restructuring

--- go/votes-service/go.mod
+++ go/votes-service/go.mod
@@ -2,4 +2,7 @@ module github.com/mcasperson/OctoPub/go/votes-service
 
 go 1.17
 
-require github.com/aws/aws-lambda-go v1.27.0
+require (
+	github.com/aws/aws-lambda-go v1.27.0
+	github.com/google/jsonapi v1.0.0
+)
---
+++ go/votes-service/pkg/httphandler/httphandler.go
@@ -0,0 +1,76 @@
+package httphandler
+
+import (
+	"github.com/google/jsonapi"
+	"github.com/mcasperson/OctoPub/go/votes-service/pkg/models"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+const (
+	headerAccept      = "Accept"
+	headerContentType = "Content-Type"
+)
+
+type HttpHandler struct{}
+
+func (h *HttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get(headerAccept) != jsonapi.MediaType {
+		http.Error(w, "Unsupported Media Type", http.StatusUnsupportedMediaType)
+	}
+
+	var methodHandler http.HandlerFunc
+	switch r.Method {
+	case http.MethodGet:
+		if r.FormValue("id") != "" {
+			methodHandler = h.showVote
+		} else {
+			methodHandler = h.listVotes
+		}
+	default:
+		http.Error(w, "Not Found", http.StatusNotFound)
+		return
+	}
+
+	methodHandler(w, r)
+}
+
+func (h *HttpHandler) showVote(w http.ResponseWriter, r *http.Request) {
+	id := r.FormValue("id")
+
+	intID, err := strconv.Atoi(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	jsonapiRuntime := jsonapi.NewRuntime().Instrument("votes.show")
+
+	// but, for now
+	vote := models.Vote{
+		ID:        intID,
+		CreatedAt: time.Time{},
+		IPAddress: "",
+		Product:   nil,
+	}
+	w.WriteHeader(http.StatusOK)
+
+	w.Header().Set(headerContentType, jsonapi.MediaType)
+	if err := jsonapiRuntime.MarshalPayload(w, vote); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (h *HttpHandler) listVotes(w http.ResponseWriter, r *http.Request) {
+	jsonapiRuntime := jsonapi.NewRuntime().Instrument("blogs.list")
+
+	votes := []models.Vote{{ID: 1, CreatedAt: time.Time{}}}
+
+	w.Header().Set("Content-Type", jsonapi.MediaType)
+	w.WriteHeader(http.StatusOK)
+
+	if err := jsonapiRuntime.MarshalPayload(w, votes); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
