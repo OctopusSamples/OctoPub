9194b7a774f6e3fd799e30e0666d8d6d79829d44
Matthew Casperson <matthewcasperson@gmail.com>
2021-11-10 15:16:55 +1000

    Fixing up native image config

--- java/products-service/src/main/java/com/octopus/octopub/config/MyReflectionConfiguration.java
+++ java/products-service/src/main/java/com/octopus/octopub/config/MyReflectionConfiguration.java
@@ -1,10 +1,10 @@
 package com.octopus.octopub.config;
 
 import com.github.jasminb.jsonapi.StringIdHandler;
-import com.github.jasminb.jsonapi.annotations.Id;
+import com.octopus.octopub.models.Product;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
-@RegisterForReflection(targets={ StringIdHandler.class, Id.class })
+@RegisterForReflection(targets={ StringIdHandler.class, Product.class })
 public class MyReflectionConfiguration {
 
 }
