8e95bdfccb8dd3bf9be4c5b41ce9b15dc495066b
Matthew Casperson <matthewcasperson@gmail.com>
2021-11-10 19:10:46 +1000

    Fixed imports

--- java/products-service/src/main/java/com/octopus/octopub/lambda/DatabaseInit.java
+++ java/products-service/src/main/java/com/octopus/octopub/lambda/DatabaseInit.java
@@ -1,8 +1,5 @@
 package com.octopus.octopub.lambda;
 
-import static javax.transaction.Transactional.TxType.NEVER;
-import static javax.transaction.Transactional.TxType.NOT_SUPPORTED;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import java.sql.Connection;
@@ -10,9 +7,7 @@ import java.sql.SQLException;
 import java.util.Map;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.persistence.EntityManager;
 import javax.sql.DataSource;
-import javax.transaction.Transactional;
 import liquibase.Contexts;
 import liquibase.LabelExpression;
 import liquibase.Liquibase;
@@ -21,7 +16,6 @@ import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
-import org.hibernate.Session;
 
 @Named("DatabaseInit")
 public class DatabaseInit implements RequestHandler<Map<String, Object>, ProxyResponse> {
