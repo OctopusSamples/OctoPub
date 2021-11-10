3699e9c8c4c06c5365871312b3a7acfe88a298c5
Matthew Casperson <matthewcasperson@gmail.com>
2021-11-10 20:02:41 +1000

    Renamed table

--- java/products-service/src/main/java/com/octopus/octopub/models/Product.java
+++ java/products-service/src/main/java/com/octopus/octopub/models/Product.java
@@ -5,11 +5,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Data;
 import org.hibernate.envers.Audited;
 
 @Entity
 @Data
+@Table(name = "product")
 @Type("products")
 public class Product {
 
