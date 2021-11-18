59177016177400e3e6b27de7fc3d79df654ab86a
Matthew Casperson <matthewcasperson@gmail.com>
2021-11-18 10:51:48 +1000

    Use standard JSONAPI endpoints

--- java/products-service/src/main/java/com/octopus/octopub/services/AuditService.java
+++ java/products-service/src/main/java/com/octopus/octopub/services/AuditService.java
@@ -8,7 +8,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-@Path("message")
+@Path("api")
 @RegisterRestClient
 public interface AuditService {
   @Path("audits")
