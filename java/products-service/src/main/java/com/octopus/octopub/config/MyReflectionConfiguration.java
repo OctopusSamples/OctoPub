89e0e7f7c49c5e579cc69c58885eab6ffd9b84e4
Matthew Casperson <matthewcasperson@gmail.com>
2021-11-19 10:18:21 +1000

    Fixed up reflection

--- java/products-service/src/main/java/com/octopus/octopub/config/MyReflectionConfiguration.java
+++ java/products-service/src/main/java/com/octopus/octopub/config/MyReflectionConfiguration.java
@@ -1,11 +1,12 @@
 package com.octopus.octopub.config;
 
+import com.github.jasminb.jsonapi.IntegerIdHandler;
 import com.github.jasminb.jsonapi.StringIdHandler;
 import com.octopus.octopub.models.Audit;
 import com.octopus.octopub.models.Product;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
-@RegisterForReflection(targets={ StringIdHandler.class, Product.class, Audit.class })
+@RegisterForReflection(targets={ StringIdHandler.class, Product.class, Audit.class, IntegerIdHandler.class })
 public class MyReflectionConfiguration {
 
 }
