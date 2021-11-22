af6c31ef62e96d6032e9bba384e1c0af55ad6fa5
Matthew Casperson <matthewcasperson@gmail.com>
2021-11-23 09:48:20 +1000

    Fixing some styling

--- js/src/pages/Settings.tsx
+++ js/src/pages/Settings.tsx
@@ -4,9 +4,15 @@ import {Helmet} from "react-helmet";
 import {createStyles, FormLabel, Grid, makeStyles, TextField} from "@material-ui/core";
 import {AppContext} from "../App";
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme) =>
     createStyles({
-        label: {}
+        container: {
+            height: "fit-content",
+            color: theme.palette.text.primary
+        },
+        cell: {
+            padding: "8px"
+        }
     })
 );
 
@@ -24,21 +30,21 @@ const Settings: FC<CommonProps> = (props: CommonProps): ReactElement => {
                     {context.settings.title}
                 </title>
             </Helmet>
-            <Grid container={true}>
-                <Grid className={classes.label} md={2} sm={12} xs={12}>
+            <Grid container={true} className={classes.container}>
+                <Grid className={classes.cell} md={2} sm={12} xs={12}>
                     <FormLabel>API Key</FormLabel>
                 </Grid>
-                <Grid item md={10} sm={12} xs={12}>
+                <Grid className={classes.cell} item md={10} sm={12} xs={12}>
                     <TextField id="apiKey" fullWidth={true} type="password" variant="outlined" value={apiKey}
                                onChange={v => {
                                    setApiKey(v.target.value);
                                    localStorage.setItem("apiKey", v.target.value);
                                }}/>
                 </Grid>
-                <Grid className={classes.label} md={2} sm={12} xs={12}>
+                <Grid className={classes.cell} md={2} sm={12} xs={12}>
                     <FormLabel>Partition</FormLabel>
                 </Grid>
-                <Grid item md={10} sm={12} xs={12}>
+                <Grid className={classes.cell} item md={10} sm={12} xs={12}>
                     <TextField id="partition" fullWidth={true} variant="outlined" value={partition}
                                onChange={v => {
                                    setPartition(v.target.value);
