69cd1f44b8a08cdf8893babc367c3e83212fb500
Matthew Casperson <matthewcasperson@gmail.com>
2021-11-24 07:27:54 +1000

    Added retries

--- js/src/utils/network.ts
+++ js/src/utils/network.ts
@@ -1,4 +1,6 @@
-export function getJsonApi<T>(url: string, partition: string | null, apiKey?: string | null): Promise<T> {
+import {GET_RETRIES} from "./constants";
+
+export function getJsonApi<T>(url: string, partition: string | null, apiKey?: string | null, retryCount?: number): Promise<T> {
     return fetch(url, {
         method: 'GET',
         headers: {
@@ -11,11 +13,18 @@ export function getJsonApi<T>(url: string, partition: string | null, apiKey?: st
             if (response.ok) {
                 return response.json();
             }
+            if (response.status === 504 && (retryCount || 0) <= GET_RETRIES) {
+                /*
+                 Some lambdas are slow, and initial requests timeout with a 504 response.
+                 We automatically retry these requests.
+                 */
+                return getJsonApi<T>(url, partition, apiKey, (retryCount || 0) + 1);
+            }
             return Promise.reject(response);
         });
 }
 
-export function patchJsonApi<T>(resource: string, url: string, partition: string | null, apiKey?: string | null): Promise<T> {
+export function patchJsonApi<T>(resource: string, url: string, partition: string | null, apiKey?: string | null, retryCount?: number): Promise<T> {
     return fetch(url, {
         method: 'PATCH',
         headers: {
@@ -28,6 +37,13 @@ export function patchJsonApi<T>(resource: string, url: string, partition: string
         .then(response => {
             if (response.ok) {
                 return response.json();
+            }
+            if (response.status === 504 && (retryCount || 0) <= GET_RETRIES) {
+                /*
+                 Some lambdas are slow, and initial requests timeout with a 504 response.
+                 We automatically retry these requests.
+                 */
+                return patchJsonApi<T>(resource, url, partition, apiKey, (retryCount || 0) + 1);
             }
             return Promise.reject(response);
         });
@@ -51,7 +67,7 @@ export function postJsonApi<T>(resource: string, url: string, partition: string 
         });
 }
 
-export function deleteJsonApi(url: string, partition: string | null, apiKey?: string | null): Promise<Response> {
+export function deleteJsonApi(url: string, partition: string | null, apiKey?: string | null, retryCount?: number): Promise<Response> {
     return fetch(url, {
         method: 'DELETE',
         headers: {
@@ -63,6 +79,13 @@ export function deleteJsonApi(url: string, partition: string | null, apiKey?: st
         .then(response => {
             if (!response.ok) {
                 return Promise.reject(response);
+            }
+            if (response.status === 504 && (retryCount || 0) <= GET_RETRIES) {
+                /*
+                 Some lambdas are slow, and initial requests timeout with a 504 response.
+                 We automatically retry these requests.
+                 */
+                return deleteJsonApi(url, partition, apiKey, (retryCount || 0) + 1);
             }
             return response;
         });
