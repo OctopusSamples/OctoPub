971ffeb47128a80778c2a679125b08d207fe26dc
Matthew Casperson <matthewcasperson@gmail.com>
2021-11-30 12:05:12 +1000

    Added tests for health checks and sqs messages

--- dotnet/Audit.Service.Tests/RequestWrapperTests.cs
+++ dotnet/Audit.Service.Tests/RequestWrapperTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.SQSEvents;
 using Audit.Service.Lambda;
 using NUnit.Framework;
 
@@ -26,6 +27,60 @@ namespace Audit.Service.Tests
         }
 
         [Test]
+        public void RequestWrapperFromHttpHealthGet()
+        {
+            var wrapper = RequestWrapperFactory.CreateFromHttpRequest(new APIGatewayProxyRequest()
+            {
+                Path = "/health/audits/GET",
+                HttpMethod = "GET",
+                Headers = new Dictionary<string, string>()
+                {
+                    { "Accept", "application/vnd.api+json" }
+                }
+            });
+
+            Assert.AreEqual(EntityType.Health, wrapper.EntityType);
+            Assert.AreEqual(ActionType.Read, wrapper.ActionType);
+            Assert.AreEqual("main", wrapper.DataPartition);
+        }
+
+        [Test]
+        public void RequestWrapperFromHttpHealthCreate()
+        {
+            var wrapper = RequestWrapperFactory.CreateFromHttpRequest(new APIGatewayProxyRequest()
+            {
+                Path = "/health/audits/POST",
+                HttpMethod = "GET",
+                Headers = new Dictionary<string, string>()
+                {
+                    { "Accept", "application/vnd.api+json" }
+                }
+            });
+
+            Assert.AreEqual(EntityType.Health, wrapper.EntityType);
+            Assert.AreEqual(ActionType.Read, wrapper.ActionType);
+            Assert.AreEqual("main", wrapper.DataPartition);
+        }
+
+        [Test]
+        public void RequestWrapperFromHttpHealthGetOne()
+        {
+            var wrapper = RequestWrapperFactory.CreateFromHttpRequest(new APIGatewayProxyRequest()
+            {
+                Path = "/health/audits/1/GET",
+                HttpMethod = "GET",
+                Headers = new Dictionary<string, string>()
+                {
+                    { "Accept", "application/vnd.api+json" }
+                }
+            });
+
+            Assert.AreEqual(EntityType.Health, wrapper.EntityType);
+            Assert.AreEqual(ActionType.Read, wrapper.ActionType);
+            Assert.AreEqual("main", wrapper.DataPartition);
+        }
+
+        [Test]
         public void RequestWrapperFromHttpGetAllMultiHeader()
         {
             var wrapper = RequestWrapperFactory.CreateFromHttpRequest(new APIGatewayProxyRequest()
@@ -34,7 +89,11 @@ namespace Audit.Service.Tests
                 HttpMethod = "GET",
                 MultiValueHeaders = new Dictionary<string, IList<string>>()
                 {
-                    { "Accept", new List<string>() {"application/vnd.api+json", "application/vnd.api+json; dataPartition=test"} }
+                    {
+                        "Accept",
+                        new List<string>()
+                            { "application/vnd.api+json", "application/vnd.api+json; dataPartition=test" }
+                    }
                 }
             });
 
@@ -95,6 +154,30 @@ namespace Audit.Service.Tests
             Assert.AreEqual(EntityType.Audit, wrapper.EntityType);
             Assert.AreEqual(ActionType.Update, wrapper.ActionType);
             Assert.AreEqual("main", wrapper.DataPartition);
+        }
+
+        [Test]
+        public void RequestWrapperFromSqsGetAll()
+        {
+            var wrapper = RequestWrapperFactory.CreateFromSqsMessage(new SQSEvent.SQSMessage()
+            {
+                MessageAttributes = new Dictionary<string, SQSEvent.MessageAttribute>()
+                {
+                    { "action", new SQSEvent.MessageAttribute() { StringValue = "Create" } },
+                    { "entity", new SQSEvent.MessageAttribute() { StringValue = "Audit" } },
+                    {
+                        "dataPartition",
+                        new SQSEvent.MessageAttribute()
+                        {
+                            StringValue = "application/vnd.api+json, application/vnd.api+json; dataPartition=testing"
+                        }
+                    }
+                }
+            });
+
+            Assert.AreEqual(EntityType.Audit, wrapper.EntityType);
+            Assert.AreEqual(ActionType.Create, wrapper.ActionType);
+            Assert.AreEqual("testing", wrapper.DataPartition);
         }
     }
 }
