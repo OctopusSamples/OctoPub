ff7c91aa7b248c1278705ee143da28d04be2aa57
Matthew Casperson <matthewcasperson@gmail.com>
2021-12-02 14:11:34 +1000

    Added checks of the HTTP Health endpoints

---
+++ java/products-service/src/test/java/com/octopus/octopub/HttpHealthTest.java
@@ -0,0 +1,42 @@
+package com.octopus.octopub;
+
+import static io.restassured.RestAssured.given;
+
+import com.github.jasminb.jsonapi.ResourceConverter;
+import com.github.jasminb.jsonapi.exceptions.DocumentSerializationException;
+import com.octopus.octopub.services.LiquidbaseUpdater;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.response.ValidatableResponse;
+import java.sql.SQLException;
+import javax.inject.Inject;
+import liquibase.exception.LiquibaseException;
+import lombok.NonNull;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@QuarkusTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class HttpHealthTest extends BaseTest {
+
+  @Inject LiquidbaseUpdater liquidbaseUpdater;
+
+  @BeforeAll
+  public void setup() throws SQLException, LiquibaseException {
+    liquidbaseUpdater.update();
+  }
+
+  @ParameterizedTest
+  @ValueSource(
+      strings = {
+        "/health/products/GET",
+        "/health/products/POST",
+        "/health/products/x/GET",
+        "/health/products/x/DELETE",
+        "/health/products/x/PATCH"
+      })
+  public void testCreateAndGetProduct(@NonNull final String path) {
+    given().when().get(path).then().statusCode(200);
+  }
+}
