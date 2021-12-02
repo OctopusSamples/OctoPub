16fd04fc410aed6471a553a042df2a8ceb373125
Matthew Casperson <matthewcasperson@gmail.com>
2021-12-03 06:56:45 +1000

    Remove some static variables

--- dotnet/Audit.Service/Lambda/Audits.cs
+++ dotnet/Audit.Service/Lambda/Audits.cs
@@ -22,7 +22,6 @@ namespace Audit.Service.Lambda
     public class Audits
     {
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-        private static readonly DependencyInjection DependencyInjection = new DependencyInjection();
 
         /// <summary>
         ///     This is the entry point to the Lambda to run database migrations.
@@ -34,7 +33,7 @@ namespace Audit.Service.Lambda
         {
             try
             {
-                var serviceProvider = DependencyInjection.ConfigureServices();
+                var serviceProvider = new DependencyInjection().ConfigureServices();
                 var db = serviceProvider.GetRequiredService<Db>();
                 db.Database.Migrate();
                 return new APIGatewayProxyResponse
@@ -58,7 +57,7 @@ namespace Audit.Service.Lambda
         {
             try
             {
-                var serviceProvider = DependencyInjection.ConfigureServices();
+                var serviceProvider = new DependencyInjection().ConfigureServices();
                 var requestWrapper = RequestWrapperFactory.CreateFromHttpRequest(request);
                 var handler = serviceProvider.GetRequiredService<AuditHandler>();
                 return AddCors(ProcessRequest(handler, requestWrapper));
@@ -79,7 +78,7 @@ namespace Audit.Service.Lambda
             Logger.Debug("Audits.HandleSqsEvent(SQSEvent, ILambdaContext)");
             Logger.Debug(sqsEvent.Records.Count + " records to process");
 
-            var serviceProvider = DependencyInjection.ConfigureServices();
+            var serviceProvider = new DependencyInjection().ConfigureServices();
             sqsEvent.Records
                 .Select(m => new Thread(() =>
                 {
