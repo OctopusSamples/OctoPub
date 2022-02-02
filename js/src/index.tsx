7994adb4514d3ea416113127548a15c8bc5d7f20
Matthew Casperson <matthewcasperson@gmail.com>
2022-02-03 07:59:40 +1000

    Handle login errors

--- js/src/index.tsx
+++ js/src/index.tsx
@@ -30,9 +30,21 @@ reportWebVitals();
 /**
  * Deal with a redirection from Cognito, handing a second redirect to the original feature branch
  * that initiated the login.
+ * @return true if the app should continue loading normally, false if we're redirecting away from this page
  */
 function handleLogin() {
     try {
+        /*
+         In the event of a login error, clear the hash and open the main page.
+         Without this, the React router may try to match the hash, which won't work.
+         */
+        const error = getHashField("error");
+
+        if (error) {
+            window.location.href = "/";
+            return false;
+        }
+
         const loginBranch = getLoginBranch();
 
         // Before we redirect to cognito, the login branch must be set. If not, ignore redirect.
@@ -40,19 +52,7 @@ function handleLogin() {
             return true;
         }
 
-        const accessToken =
-            // The returned hash from Cognito splits id and access tokens with ampersand
-            window.location.hash.split("&")
-                // The access token starts with this string
-                .filter(h => h.startsWith("access_token"))
-                // The tokens are name=value
-                .map(h => h.split("="))
-                // sanity check to make sure we have 2 values
-                .filter(h => h.length === 2)
-                // get the value
-                .map(h => h.pop())
-                // there should only be one element
-                .pop();
+        const accessToken = getHashField("access_token");
 
         if (accessToken) {
             setAccessToken(accessToken);
@@ -64,6 +64,21 @@ function handleLogin() {
     } finally {
         clearLoginBranch();
     }
+}
+
+function getHashField(field: string) {
+    // The returned hash from Cognito splits id and access tokens with ampersand
+    return window.location.hash.split("&")
+        // The access token starts with this string
+        .filter(h => h.startsWith(field))
+        // The tokens are name=value
+        .map(h => h.split("="))
+        // sanity check to make sure we have 2 values
+        .filter(h => h.length === 2)
+        // get the value
+        .map(h => h.pop())
+        // there should only be one element
+        .pop();
 }
 
 function setupGoogleAnalytics(tag: string) {
