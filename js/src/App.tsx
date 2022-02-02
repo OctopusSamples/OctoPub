ab95c84f9e3e811172bd5705a424c6dfaf02d9fc
Matthew Casperson <matthewcasperson@gmail.com>
2022-02-02 16:05:03 +1000

    Fixing up routing paths

--- js/src/App.tsx
+++ js/src/App.tsx
@@ -84,7 +84,7 @@ function App(config: DynamicConfig) {
                                     route.subRoutes.map((item: RouteItem) => (
                                         <Route
                                             key={`${item.key}`}
-                                            path={`${item.path}`}
+                                            path={`${process.env.PUBLIC_URL}/${item.path}`}
                                             element={<Layout toggleTheme={toggle} useDefaultTheme={useDefaultTheme}>
                                                 {item.component()({})}</Layout>}
                                         />
@@ -92,7 +92,7 @@ function App(config: DynamicConfig) {
                                 ) : (
                                     <Route
                                         key={`${route.key}`}
-                                        path={`${route.path}`}
+                                        path={`${process.env.PUBLIC_URL}/${route.path}`}
                                         element={<Layout toggleTheme={toggle}
                                                          useDefaultTheme={useDefaultTheme}>{route.component()({})}</Layout>}
                                     />
