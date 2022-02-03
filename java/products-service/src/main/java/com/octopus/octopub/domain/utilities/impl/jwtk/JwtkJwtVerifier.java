a4b098e346164b794b5b208f0026407871b76e46
Matthew Casperson <matthewcasperson@gmail.com>
2022-02-03 16:58:55 +1000

    Made some env vars optional

--- java/products-service/src/main/java/com/octopus/octopub/domain/utilities/impl/jwtk/JwtkJwtVerifier.java
+++ java/products-service/src/main/java/com/octopus/octopub/domain/utilities/impl/jwtk/JwtkJwtVerifier.java
@@ -5,15 +5,14 @@ import com.octopus.octopub.domain.utilities.JwtVerifier;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+import java.util.Optional;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import lombok.NonNull;
 import org.apache.commons.lang3.ArrayUtils;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-/**
- * An implementation of JwtVerifier using Jwtk.
- */
+/** An implementation of JwtVerifier using Jwtk. */
 @ApplicationScoped
 public class JwtkJwtVerifier implements JwtVerifier {
 
@@ -21,33 +20,40 @@ public class JwtkJwtVerifier implements JwtVerifier {
 
   @Inject
   @ConfigProperty(name = "cognito.pool")
-  String cognitoPool;
+  Optional<String> cognitoPool;
 
   @Inject
   @ConfigProperty(name = "cognito.region")
-  String cognitoRegion;
+  Optional<String> cognitoRegion;
+
+  @Inject
+  @ConfigProperty(name = "cognito.disable-auth")
+  Boolean cognitoDisableAuth;
 
   private final JwkKeyResolver jwkKeyResolver;
 
-  /**
-   * Constructor.
-   */
+  /** Constructor. */
   public JwtkJwtVerifier() {
+
     this.jwkKeyResolver =
-        new JwkKeyResolver(
-            new UrlJwkProvider(
-                "https://cognito-idp."
-                    + cognitoRegion
-                    + ".amazonaws.com/"
-                    + cognitoPool
-                    + "/.well-known/jwks.json"));
+        cognitoDisableAuth
+            ? null
+            : new JwkKeyResolver(
+                new UrlJwkProvider(
+                    "https://cognito-idp."
+                        + cognitoRegion
+                        + ".amazonaws.com/"
+                        + cognitoPool
+                        + "/.well-known/jwks.json"));
   }
 
-  /**
-   * {@inheritDoc}
-   */
+  /** {@inheritDoc} */
   @Override
   public boolean jwtContainsCognitoGroup(@NonNull final String jwt, @NonNull final String group) {
+    if (cognitoDisableAuth) {
+      return false;
+    }
+
     final Jws<Claims> claims =
         Jwts.parserBuilder().setSigningKeyResolver(jwkKeyResolver).build().parseClaimsJws(jwt);
     if (claims.getBody().containsKey(COGNITO_GROUPS)) {
