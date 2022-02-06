284df5c917a1e0cce412a73dd09ab10eaa822de7
Matthew Casperson <matthewcasperson@gmail.com>
2022-02-07 09:04:44 +1000

    Fixed checkstyle errors

--- java/products-service/src/main/java/com/octopus/octopub/configuration/ReflectionConfiguration.java
+++ java/products-service/src/main/java/com/octopus/octopub/configuration/ReflectionConfiguration.java
@@ -2,7 +2,6 @@ package com.octopus.octopub.configuration;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
+/** Ensure any classes reflected at runtime are bundled into the native build. */
 @RegisterForReflection(targets = {io.jsonwebtoken.impl.DefaultJwtParserBuilder.class})
-public class ReflectionConfiguration {
-
-}
+public class ReflectionConfiguration {}
