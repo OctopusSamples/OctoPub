197d704424d45b7a91b88d5f657069e804ee11d4
Matthew Casperson <matthewcasperson@gmail.com>
2022-02-07 09:27:51 +1000

    Registered more classes for reflection

--- java/products-service/src/main/java/com/octopus/octopub/configuration/ReflectionConfiguration.java
+++ java/products-service/src/main/java/com/octopus/octopub/configuration/ReflectionConfiguration.java
@@ -3,5 +3,8 @@ package com.octopus.octopub.configuration;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
 /** Ensure any classes reflected at runtime are bundled into the native build. */
-@RegisterForReflection(targets = {io.jsonwebtoken.impl.DefaultJwtParserBuilder.class})
+@RegisterForReflection(targets = {io.jsonwebtoken.impl.DefaultJwtParserBuilder.class,
+    io.jsonwebtoken.impl.compression.DefaultCompressionCodecResolver.class,
+    io.jsonwebtoken.impl.compression.DeflateCompressionCodec.class,
+    io.jsonwebtoken.impl.compression.GzipCompressionCodec.class})
 public class ReflectionConfiguration {}
