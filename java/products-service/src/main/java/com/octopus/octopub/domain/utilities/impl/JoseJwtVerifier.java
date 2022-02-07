9d4c888e5f482f7e0dd2d192d4d9ce1dad1cbad4
Matthew Casperson <matthewcasperson@gmail.com>
2022-02-07 20:32:16 +1000

    Cleaned up logging

--- java/products-service/src/main/java/com/octopus/octopub/domain/utilities/impl/JoseJwtVerifier.java
+++ java/products-service/src/main/java/com/octopus/octopub/domain/utilities/impl/JoseJwtVerifier.java
@@ -65,7 +65,7 @@ public class JoseJwtVerifier implements JwtVerifier {
         }
       }
     } catch (final IOException | ParseException | JOSEException e) {
-      Log.error(GlobalConstants.MICROSERVICE_NAME + "-Jwt-ValidationError Failed to parse JWT", e);
+      Log.error(GlobalConstants.MICROSERVICE_NAME + "-Jwt-ValidationError", e);
     }
 
     return false;
