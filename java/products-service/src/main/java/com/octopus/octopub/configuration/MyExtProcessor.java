7ecdd36552b0dffc5daf3d6c9a489e966c743cc8
Matthew Casperson <matthewcasperson@gmail.com>
2022-02-07 19:01:01 +1000

    Trying the example code directly

--- java/products-service/src/main/java/com/octopus/octopub/configuration/MyExtProcessor.java
+++ java/products-service/src/main/java/com/octopus/octopub/configuration/MyExtProcessor.java
@@ -8,28 +8,29 @@ import io.quarkus.logging.Log;
 import java.io.IOException;
 import java.util.Set;
 
+/** Registering classes to be loaded as services. */
 public final class MyExtProcessor {
   @BuildStep
   void registerNativeImageResources(final BuildProducer<ServiceProviderBuildItem> services)
       throws IOException {
     /*
-      Taken from https://quarkus.io/guides/writing-extensions#service-files.
-      We need to expose the JWT library services.
-     */
+     Taken from https://quarkus.io/guides/writing-extensions#service-files.
+     We need to expose the JWT library services.
+    */
 
     final String service = "META-INF/services/" + io.jsonwebtoken.CompressionCodec.class.getName();
 
     // find out all the implementation classes listed in the service files
     final Set<String> implementations =
-        ServiceUtil.classNamesNamedIn(Thread.currentThread().getContextClassLoader(),
-            service);
+        ServiceUtil.classNamesNamedIn(Thread.currentThread().getContextClassLoader(), service);
 
     Log.debug("Registering CompressionCodec implementations: " + implementations);
 
     // register every listed implementation class so they can be instantiated
     // in native-image at run-time
     services.produce(
-        new ServiceProviderBuildItem(io.jsonwebtoken.CompressionCodec.class.getName(),
+        new ServiceProviderBuildItem(
+            io.jsonwebtoken.CompressionCodec.class.getName(),
             implementations.toArray(new String[0])));
   }
 }
