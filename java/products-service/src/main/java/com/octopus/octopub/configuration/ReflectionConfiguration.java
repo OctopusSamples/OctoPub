3ba988e8013289a6b1be5fc69a4ca981972221a5
Matthew Casperson <matthewcasperson@gmail.com>
2022-02-07 15:53:24 +1000

    Adding a service file manually to try and fix JWT compression class loading

--- java/products-service/src/main/java/com/octopus/octopub/configuration/ReflectionConfiguration.java
+++ java/products-service/src/main/java/com/octopus/octopub/configuration/ReflectionConfiguration.java
@@ -5,7 +5,6 @@ import io.quarkus.runtime.annotations.RegisterForReflection;
 /** Ensure any classes reflected at runtime are bundled into the native build. */
 @RegisterForReflection(targets = {io.jsonwebtoken.impl.DefaultJwtParserBuilder.class,
     io.jsonwebtoken.impl.compression.DefaultCompressionCodecResolver.class,
-    io.jsonwebtoken.CompressionCodec.class,
     io.jsonwebtoken.impl.compression.DeflateCompressionCodec.class,
     io.jsonwebtoken.impl.compression.GzipCompressionCodec.class})
 public class ReflectionConfiguration {}
